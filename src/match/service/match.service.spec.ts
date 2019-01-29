49db064d9d7779de1ab65ffd0a997e6edb48bd61
Miguel López <miguelopezv@gmail.com>
2019-01-29 11:12:54 -0500

    Services unit test

--- src/match/service/match.service.spec.ts
+++ src/match/service/match.service.spec.ts
@@ -1,18 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MatchService } from './match.service';
+import { MatchRepository } from '../mocks/match.repository';
+import { Match } from '../entity/match.entity';
 
+class MockMatchService {
+  getMatches(id1: number, id2: number): Promise<object> {
+    const result = {
+      player1: { id: 1, wins: 4 },
+      player2: { id: 2, wins: 8 },
+    };
+
+    return new Promise<object>((resolve) => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+  }
+
+  saveMatch(body: any): Promise<Match> {
+    const result =  {winnerPlayer: 6, loserPlayer: 4, id: 13, createdAt: new Date()};
+
+    return new Promise<Match>(resolve => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+  }
+
+  getDetailedMatches(id1: number, id2: number, page: number): Promise<object> {
+    const result = { page: 2, pageCount: 2, data: [] };
+
+    return new Promise<object>(resolve => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+
+  }
+}
+
+let mockService: MockMatchService;
 describe('MatchService', () => {
-  let service: MatchService;
 
   beforeEach(async () => {
+    mockService = new MockMatchService();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MatchService],
+      providers: [MatchService, {
+        provide: 'MatchRepository',
+        useValue: MatchRepository,
+      }],
     }).compile();
 
-    service = module.get<MatchService>(MatchService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(mockService).toBeDefined();
+  });
+
+  it('should return Matches', async () => {
+    const result = {
+      player1: { id: 1, wins: 4 },
+      player2: { id: 2, wins: 8 },
+    };
+
+    mockService.getMatches(1, 2).then(res => {
+      expect(res).toEqual(result);
+    });
+  });
+
+  it('should be save a match', async () => {
+    const result = {
+      winnerPlayer: 6,
+      loserPlayer: 4,
+      id: 13,
+      createdAt: new Date(),
+    };
+
+    mockService.saveMatch({ body: { winnerPlayer: 6, loserPlayer: 4 } }).then(res =>{
+      expect(res).toEqual(result);
+    });
+  });
+
+  it('should return detailed matches', async () => {
+    const result = {
+      page: 2,
+      pageCount: 2,
+      data: [],
+    };
+
+    mockService.getDetailedMatches(1, 2, 2).then(res => {
+      expect(res).toEqual(result);
+    });
   });
 });
--- src/player/service/player.service.spec.ts
+++ src/player/service/player.service.spec.ts
@@ -1,19 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayerService } from './player.service';
+import { PlayerRepository } from '../mocks/player.repository';
+import { Player } from '../entity/player.entity';
 
+class MockPlayerService {
+  findPlayer({ firstName, lastName }): Promise<Player> {
+    return new Promise<Player>((resolve) => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+  }
+
+  save({ firstName, lastName }): Promise<Player> {
+    return new Promise<Player>((resolve) => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+  }
+}
+
+const result: Player = {
+  id: 1,
+  firstName: 'Miguel',
+  lastName: 'Lopez',
+  wonMatches: [],
+  losedMatches: [],
+};
+
+let mockService: MockPlayerService;
 describe('PlayerService', () => {
-  let service: PlayerService;
 
   beforeEach(async () => {
+    mockService = new MockPlayerService();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerService],
+      providers: [
+        PlayerService,
+        {
+          provide: 'PlayerRepository',
+          useValue: PlayerRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<PlayerService>(PlayerService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(mockService).toBeDefined();
   });
 
+  it('should find a player', async () => {
+    mockService.findPlayer({firstName: 'Miguel', lastName: 'Lopez'}).then(res => {
+      expect(res).toEqual(result);
+    });
+  });
+
+  it('should save a player', async () => {
+    mockService.save({ firstName: 'Miguel', lastName: 'Lopez' }).then(res => {
+      expect(res).toEqual(result);
+    });
+  });
 });
