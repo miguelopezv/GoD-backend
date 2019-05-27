83186d9e67c245b527947d61662df8d4c8c59419
Miguel López <miguelopezv@gmail.com>
2019-05-27 17:28:35 -0500

    add MongooseModule

--- src/match/match.module.ts
+++ src/match/match.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchController } from './match.controller';
 import { MatchService } from './service/match.service';
-import { Match } from './entity/match.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MatchSchema } from './schemas/match.schema';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Match', schema: MatchSchema }]),
+  ],
   controllers: [MatchController],
   providers: [MatchService],
 })
