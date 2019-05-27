5f423073d3374ec88fa5df2c19dbf5419f094417
Miguel López <miguelopezv@gmail.com>
2019-05-27 14:22:02 -0500

    remove TypeORM and add mongoose

--- src/player/mocks/player.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Player } from '../entity/player.entity';
-
-@EntityRepository(Player)
-export class PlayerRepository extends Repository<Player> { }
